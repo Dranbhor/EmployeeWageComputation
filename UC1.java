5cb25204839425a0ebd88b8bb19b63d31d5dcbe1
Dhanashree <ranbhor.dhanashree37@gmail.com>
2021-09-10 23:24:21 +0530

    UC1

---
+++ UC1.java
@@ -0,0 +1,17 @@
+package Assignment1;
+
+public class UC1
+{
+	public static void main(String args[])
+	   {	
+
+		   int IS_FULL_TIME_ = 2;
+		   
+		   double empcheck = Math.floor(Math.random() * 10) % 2;
+		   
+		   if(empcheck == IS_FULL_TIME_)
+		       System.out.println("Employee Is Present");
+		   else
+			   System.out.println("Employee Is Absent");
+	   }
+}
