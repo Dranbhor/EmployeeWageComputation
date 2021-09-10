d9b0020caec4b1467f6bd25170ff87170ad05dea
Dhanashree <ranbhor.dhanashree37@gmail.com>
2021-09-10 23:27:25 +0530

    Second Commit

---
+++ UC2.java
@@ -0,0 +1,25 @@
+package Assignment1;
+
+public class UC2
+{
+
+	public static void main(String[] args)
+	{
+		final int IS_FULL_TIME = 1;
+		final int EMP_RATE_PER_HOUR = 20;
+		
+		int empHours = 0;
+		int empWage = 0;
+		
+		double empcheck = Math.floor(Math.random() * 10 % 2);
+		if(empcheck == IS_FULL_TIME)
+			empHours = 8;
+		else 
+		   empWage = EMP_RATE_PER_HOUR * empHours;
+		
+		System.out.println("Employee Wage :" + empWage);
+		
+
+	}
+
+}
